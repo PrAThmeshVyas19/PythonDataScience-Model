16785fe31ac86c12f29c788c132e7553f14b1f47
Prathmesh Vyas <65387358+PrAThmeshVyas19@users.noreply.github.com>
2022-12-15 23:43:15 +0530

    Add files via upload
    IrisCaseStudy1 - Model : KNN
    IrisCaseStudy2 - Model : DecisionTree

---
+++ IrisCaseStudy1.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+def MarvellousNeighborsClassifier():
+    Dataset = load_iris()   # 1 Load the data
+
+    Data = Dataset.data
+    Target = Dataset.target
+
+    # 2 : Manipulate the data
+    Data_train , Data_test , Target_train , Target_test = train_test_split(Data , Target , test_size = 0.5) 
+
+    Classifier = KNeighborsClassifier()
+
+    # 3 : build the model
+    Classifier.fit(Data_train , Target_train)
+
+    # 4 : Test the mode
+    Predictions = Classifier.predict(Data_test)
+
+    Accurracy = accuracy_score(Target_test , Predictions)
+
+    # 5 : Improve --> Missing
+    return Accurracy
+
+def main():
+    Ret = MarvellousNeighborsClassifier()
+
+    print("Accuracy of Iris dataset with KNN is : ",Ret * 100)
+
+
+if __name__ == "__main__":
+    main()
